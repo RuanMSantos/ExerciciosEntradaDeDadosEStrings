27f1da01cd9eb0438a6d3715c187b45ca46347e9
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-17 22:33:08 -0300

    Exercício finalizado OlaUsuario

---
+++ OlaUsuario/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+ 
+Console.Write("Digite seu nome: ");
+string nome = Console.ReadLine()!;
+ 
+Console.Write("Olá ");
+ 
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine($"{nome}");
+Console.ResetColor();
