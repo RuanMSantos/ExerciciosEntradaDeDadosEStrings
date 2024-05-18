5896e49de8a526c795208ec76a79f781c1ca4d7d
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-17 22:36:48 -0300

    Exercício finalizado Gritador

---
+++ Gritador/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+ 
+Console.WriteLine("----- Gritador -----");
+ 
+Console.Write("\nDigite algo: ");
+string gritando = Console.ReadLine()!.ToUpper();
+ 
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine($"\n{gritando}");
+Console.ResetColor();
