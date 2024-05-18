b77c082f1c41e19d8f780433de5ebf4ada327a21
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-17 22:39:36 -0300

    Exercício finalizado NomeCompleto

---
+++ NomeCompleto/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+ 
+string nome, sobrenome;
+ 
+Console.WriteLine("----- Nome e Sobrenome -----");
+ 
+Console.Write("\nDigite seu primeiro nome: ");
+nome = Console.ReadLine()!;
+ 
+Console.Write("\nDigite seu sobrenome: ");
+sobrenome = Console.ReadLine()!;
+ 
+Console.WriteLine($"\nNome completo: {nome} {sobrenome}");
+ 
+Console.WriteLine($"\nNome de catálogo: {sobrenome.ToUpper()}, {nome}");
