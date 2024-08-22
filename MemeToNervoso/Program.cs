16acb77e7beacdad1023bbea0bb5c4cb4b0607e1
RuanMSantos <ruancauancauan@gmail.com>
2024-08-21 22:40:15 -0300

    Exercício finalizado MemeToNervoso

---
+++ MemeToNervoso/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+ 
+string resposta;
+ 
+Console.WriteLine("---- Meme To Nervoso ----");
+ 
+Console.Write("\nUsuário, o que você pensa sobre mim? ");
+resposta = Console.ReadLine()!;
+ 
+Thread.Sleep(1500);
+Console.WriteLine("\n- E sabe o que eu penso de você?");
+ 
+Thread.Sleep(3000);
+Console.WriteLine("Você é mui... NÃO, PERA.");
+ 
+Thread.Sleep(1000);
+Console.WriteLine("Tô nervoso");
