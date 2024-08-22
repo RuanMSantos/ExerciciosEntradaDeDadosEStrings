4a4810629b34911a31b1a866ba11796b7bf2211b
RuanMSantos <ruancauancauan@gmail.com>
2024-08-21 22:34:34 -0300

    Exercício finalizado CriancaEducada

---
+++ CriancaEducada/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.Clear();
+ 
+const string sensura = "#@$%*!&";
+ 
+Console.WriteLine("--- Criança Educada ---");
+ 
+Console.Write("\nMe diga o que você pensa sobre o aluno que não faz a lição de casa: ");
+string resposta = Console.ReadLine()!;
+ 
+Console.WriteLine("\nEu sou uma criança educada. No seu lugar eu diria:");
+ 
+ 
+Console.WriteLine($"{resposta}"
+.Replace("chato", sensura)
+.Replace("chata", sensura)
+.Replace("bobo", sensura)
+.Replace("boba", sensura)
+.Replace("feio", sensura)
+.Replace("feia", sensura)
+.Replace("boboca", sensura)
+.Replace("bocó", sensura)
+.Replace("tonto", sensura)
+.Replace("tonta", sensura)
+.Replace("palerma", sensura)
+.Replace("paspalho", sensura)
+.Replace("paspalha", sensura)
+.Replace("tantã", sensura)
+.Replace("panaca", sensura)
+.Replace("pentelho", sensura)
+.Replace("pentelha", sensura)
+.Replace("burro", sensura)
+.Replace("burra", sensura)
+.Replace("besta", sensura)
+ 
+.Replace("CHATO", sensura)
+.Replace("CHATA", sensura)
+.Replace("BOBO", sensura)
+.Replace("BOBA", sensura)
+.Replace("FEIO", sensura)
+.Replace("FEIA", sensura)
+.Replace("BOBOCA", sensura)
+.Replace("BOCÓ", sensura)
+.Replace("TONTO", sensura)
+.Replace("TONTA", sensura)
+.Replace("PALERMA", sensura)
+.Replace("PASPALHO", sensura)
+.Replace("PASPALHA", sensura)
+.Replace("TANTÃ", sensura)
+.Replace("PANACA", sensura)
+.Replace("PENTELHO", sensura)
+.Replace("PENTELHA", sensura)
+.Replace("BURRO", sensura)
+.Replace("BURRA", sensura)
+.Replace("BESTA", sensura)
+);
