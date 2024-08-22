39d8db783ef1988faa0722f520ed07f0083599c2
RuanMSantos <ruancauancauan@gmail.com>
2024-08-21 22:27:44 -0300

    Exercício finalizado Cebolinha

---
+++ Cebolinha/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+ 
+string frase;
+ 
+Console.WriteLine("---- Cebolinha's converter ----");
+ 
+Console.Write("\nDigite uma frase: ");
+frase = Console.ReadLine()!;
+ 
+Console.WriteLine("\nO Cebolinha diria...\n");
+ 
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(frase
+.Replace("r", "l")
+.Replace("R", "L"));
+Console.ResetColor();
+ 
