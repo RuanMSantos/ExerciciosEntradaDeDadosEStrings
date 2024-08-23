1aa0ba3043882e7f771bd03f1de814be235de075
RuanMSantos <ruancauancauan@gmail.com>
2024-08-22 22:26:11 -0300

    Exercício finalizado Leet

---
+++ Leet/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+ 
+Console.WriteLine("---- Leet Speak ----");
+ 
+Console.Write("\nTexto a ser traduzido...: ");
+string texto = Console.ReadLine()!;
+ 
+Console.Write($"TRADUZIDO: ");
+ 
+Console.WriteLine($"{texto}"
+.Replace("A", "4")
+.Replace("a", "4")
+.Replace("E", "3")
+.Replace("e", "3")
+.Replace("I", "1")
+.Replace("i", "1")
+.Replace("O", "0")
+.Replace("o", "0")
+.Replace("T", "7")
+.Replace("t", "7")
+.Replace("S", "5")
+.Replace("s", "5")
+);
