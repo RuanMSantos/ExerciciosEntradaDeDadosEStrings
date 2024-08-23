c4174d41f861e01176187c587035eece59033483
RuanMSantos <ruancauancauan@gmail.com>
2024-08-22 22:35:34 -0300

    Exercício finalizado PrimeiroCaractere

---
+++ PrimeiroCaractere/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+ 
+Console.WriteLine("---- Primeiro caractere ----");
+ 
+Console.Write("\nDigite um palavra: ");
+string palavra = Console.ReadLine()!.Trim().Substring(0, 1);
+ 
+Console.WriteLine($"Primeiro caractere: {palavra}");
