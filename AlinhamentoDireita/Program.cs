6452027c979a04fec837dae345d0f89052a52d1c
RuanMSantos <ruancauancauan@gmail.com>
2024-09-19 17:56:34 -0300

    Exercício finalizado AlinhamentoDireita

---
+++ AlinhamentoDireita/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+ 
+Console.WriteLine("--- Alinhamento à direita ---");
+ 
+Console.Write("\nDigite a primeira palavra...: ");
+string palavra1 = Console.ReadLine()!.Trim();
+ 
+Console.Write("Digite a primeira palavra...: ");
+string palavra2 = Console.ReadLine()!.Trim();
+ 
+Console.Write("Digite a primeira palavra...: ");
+string palavra3 = Console.ReadLine()!.Trim();
+ 
+Console.WriteLine($"\n{palavra1.PadLeft(20, ' ')}");
+Console.WriteLine($"{palavra2.PadLeft(20, ' ')}");
+Console.WriteLine($"{palavra3.PadLeft(20, ' ')}");
