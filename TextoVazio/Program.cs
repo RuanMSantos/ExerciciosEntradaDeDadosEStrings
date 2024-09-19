dae7a51c143dc37a48b22d218c9f6c59b8496a63
RuanMSantos <ruancauancauan@gmail.com>
2024-09-19 17:54:01 -0300

    Exercício finalizado TextoVazio

---
+++ TextoVazio/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+ 
+Console.WriteLine("--- Texto Vazio ---");
+ 
+Console.Write("\nDigite um texto qualquer: ");
+string resposta = Console.ReadLine()!;
+ 
+Console.WriteLine(string.IsNullOrEmpty(resposta.Trim()));
