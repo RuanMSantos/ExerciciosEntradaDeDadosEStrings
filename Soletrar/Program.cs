06c933fc27be571d260f774544ab3e2afb22c0a3
RuanMSantos <ruancauancauan@gmail.com>
2024-10-08 12:03:37 -0300

    Exercício finalizado Soletrar

---
+++ Soletrar/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("-- Soletrar --");
+string texto = Console.ReadLine()!;
+
+Console.WriteLine("\nSoletrando fica...\n");
+
+Console.WriteLine(string.Join("-", texto.ToUpper().ToCharArray()));
+
