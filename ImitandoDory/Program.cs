963fe55043455fff0f92b87690ce486a3b241e7a
RuanMSantos <ruancauancauan@gmail.com>
2024-10-08 12:48:45 -0300

    Exercício finalizado ImitandoDory

---
+++ ImitandoDory/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("--- Procurando Dory ---");
+
+Console.Write("\nDigite uma palavra: ");
+string palavra = Console.ReadLine()!;
+
+string conversor = palavra
+.Replace("a", "aaa")
+.Replace("e", "eee")
+.Replace("i", "iii")
+.Replace("o", "ooo")
+.Replace("u", "uuu")
+.Replace("A", "AAA")
+.Replace("E", "EEE")
+.Replace("I", "III")
+.Replace("O", "OOO")
+.Replace("U", "UUU");
+
+Console.WriteLine($"\nA Dory diria: {conversor}");
