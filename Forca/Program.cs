ad4711f697e3ee64ddf58caf676f7b7e374c3f18
RuanMSantos <ruancauancauan@gmail.com>
2024-10-08 12:21:32 -0300

    Exercício finalizado Forca

---
+++ Forca/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("--- Forca ---\n");
+
+Console.Write("Qual a palavra secreta? ");
+string palavra = Console.ReadLine()!;
+
+Console.Write("Qual a palavra secreta? ");
+string letra = Console.ReadLine()!.Trim().ToLower().Substring(0, 1);
+
+bool verificador = palavra.ToLower().Contains(letra);
+
+Console.WriteLine($"\nA letra \"{letra}\" existe na palavra secreta => {verificador}");
