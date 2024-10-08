c5b8df2e34d2e90c84b9655d24f5d9178327f0ff
RuanMSantos <ruancauancauan@gmail.com>
2024-10-08 14:53:44 -0300

    Exercício finalizado RolagemComposta

---
+++ RolagemComposta/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("--- Rolagem de Dados ---");
+
+Console.Write("\nDigite a rolagem desejada...: ");
+string resposta = Console.ReadLine()!.Trim().ToLower();
+
+Console.WriteLine($"\n{Dado(resposta)} dado(s) de {Face(resposta)} face(s) mais {Bonus(resposta)}");
+
+string Dado(string dado) => dado.Substring(0, resposta.IndexOf("d"));
+string Face(string face) => face.Substring(resposta.IndexOf("d") + 1, resposta.IndexOf("+") - resposta.IndexOf("d") - 1);
+string Bonus(string bonus) => bonus.Substring(resposta.IndexOf("+") + 1);
